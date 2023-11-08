01b55aa5a15e06499d07f893fd0f2221d62ac6b5
Mmmolina07 <mateoma1234@gmail.com>
2023-11-08 14:49:43 -0500

    avance

--- tarifas.py
+++
@@ -1,9 +0,0 @@
-class Tarifas:
-    def __init__(self):
-        self.tarifas = {}
-
-    def agregar_tarifa(self, numero_serie, tarifa_por_hora):
-        self.tarifas[numero_serie] = tarifa_por_hora
-
-    def obtener_tarifa(self, numero_serie):
-        return self.tarifas.get(numero_serie, 0)
--- usuario.py
+++
@@ -1,5 +0,0 @@
-class Usuario:
-    def __init__(self, nombre, correo, identificacion):
-        self.nombre = nombre
-        self.correo = correo
-        self.identificacion = identificacion
