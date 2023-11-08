eeaf6670b4cdf73990846296f18429190bce0394
Mmmolina07 <mateoma1234@gmail.com>
2023-11-08 15:54:12 -0500

    .#

--- app.py
+++ app.py
@@ -1,9 +1,1 @@
-from silla import Silla
-from usuario import Usuario
-from tarifas import Tarifas
-from sistemaalquiler import SistemaAlquiler
-
-def main():
-    sistema = SistemaAlquiler()
-    
 
